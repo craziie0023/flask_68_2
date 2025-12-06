b96d249cc8d4ec1497bfd15c3eedadc61208ca5f
Arucha <craziie0023@gmail.com>
2025-12-06 11:45:35 +0700

    template

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify , render_template
 
 app = Flask(__name__)
 
 @app.route('/home', methods=['GET'])
 def home():
-    return jsonify(message="welcome to the home page!")
+    # return jsonify(message="Welcome to the Home Page!")
+    
+    neme = "Anya"
+    age = 7
+    my_dict = {"name": "Yor", "age": 26}
+    # from flask import render_template
+    return render_template('home.html', name=neme, age=age, my_dict=my_dict)
+
+@app.route('/create' , methods=['GET'])
+def create():
+    return render_template('create.html')
 
 if __name__ == '__main__':
     # app.run()# production mode
